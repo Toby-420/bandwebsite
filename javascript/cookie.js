dba788ebdc6260ca61c2a59330d17f76d614cf01
Toby <83547635+Toby-420@users.noreply.github.com>
2024-02-19 20:49:00 +0000

    Update styles. Again
    They said this would be easy. Then again, they said it would be easy to stop fascists from talking

--- javascript/cookie.js
+++ javascript/cookie.js
@@ -1,19 +1,18 @@
 // Technically not cookie but who gives a shit
 let themePreference = localStorage.getItem("themePreference");
 let currentTheme = themePreference || "https://deepsealemons.co.uk/styles/light.css";
-  
+
 const hiringSVGContainer = document.getElementById("hiringSVG");
 const jobsSVGContainer = document.getElementById("jobsSVG");
 const X_Logo = document.getElementById("X_Logo");
 
-// Add an event listener for the 'load' event
 window.addEventListener("load", function () {
-	// Apply the theme based on the user's preference
-	if (themePreference != null) {
-		applyTheme(currentTheme);
-	} else {
-		applyTheme("https://deepsealemons.co.uk/styles/light.css");
-	}
+  // Apply the theme based on the user's preference
+  if (themePreference != null) {
+	applyTheme(currentTheme);
+  } else {
+	applyTheme("https://deepsealemons.co.uk/styles/light.css");
+  }
 });
 
 function toggleTheme() {
@@ -30,7 +29,7 @@ function updateThemePreference(theme) {
 function applyTheme(theme) {
   document.body.style.visibility = "hidden";
   document.getElementById("css_standard").setAttribute("href", theme);
-  
+
   if (currentTheme === "https://deepsealemons.co.uk/styles/light.css") {
 	  hiringSVGContainer.setAttribute("src", "images/icons/light-calendar.svg");
 	  jobsSVGContainer.setAttribute("src", "images/icons/light-hired.svg");
