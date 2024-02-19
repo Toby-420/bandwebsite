1368fd74c40ab663c55b611b877d315c18f45fef
Toby <83547635+Toby-420@users.noreply.github.com>
2024-02-19 19:28:52 +0000

    Update cookie.js

--- javascript/cookie.js
+++ javascript/cookie.js
@@ -1,6 +1,6 @@
 // Technically not cookie but who gives a shit
 let themePreference = localStorage.getItem("themePreference");
-let currentTheme = themePreference || "styles/light.css";
+let currentTheme = themePreference || "https://deepsealemons.co.uk/styles/light.css";
   
 const hiringSVGContainer = document.getElementById("hiringSVG");
 const jobsSVGContainer = document.getElementById("jobsSVG");
@@ -10,11 +10,11 @@ if (themePreference != null) {
 	cookiesAccepted = true;
 	applyTheme(currentTheme);
 } else {
-	applyTheme("styles/light.css");
+	applyTheme("https://deepsealemons.co.uk/styles/light.css");
 }
 
 function toggleTheme() {
-  currentTheme = currentTheme === "styles/light.css" ? "styles/dark.css" : "styles/light.css";
+  currentTheme = currentTheme === "https://deepsealemons.co.uk/styles/light.css" ? "https://deepsealemons.co.uk/styles/dark.css" : "https://deepsealemons.co.uk/styles/light.css";
   applyTheme(currentTheme);
   updateThemePreference(currentTheme);
 }
@@ -27,7 +27,7 @@ function updateThemePreference(theme) {
 function applyTheme(theme) {
   document.getElementById("css_standard").setAttribute("href", theme);
   
-  if (currentTheme === "styles/light.css") {
+  if (currentTheme === "https://deepsealemons.co.uk/styles/light.css") {
 	  hiringSVGContainer.setAttribute("src", "images/icons/light-calendar.svg");
 	  jobsSVGContainer.setAttribute("src", "images/icons/light-hired.svg");
 	  X_Logo.setAttribute("src", "images/icons/x-black.png");
