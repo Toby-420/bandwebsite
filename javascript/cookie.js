a4dafc18b4de348849ac5d8ca06e260ac10d207b
Toby <83547635+Toby-420@users.noreply.github.com>
2024-02-19 20:27:43 +0000

    Update styles. Again. Again.

--- javascript/cookie.js
+++ javascript/cookie.js
@@ -1,5 +1,4 @@
 // Technically not cookie but who gives a shit
-document.body.style.visibility = "hidden";
 let themePreference = localStorage.getItem("themePreference");
 let currentTheme = themePreference || "https://deepsealemons.co.uk/styles/light.css";
   
@@ -9,16 +8,16 @@ const X_Logo = document.getElementById("X_Logo");
 
 // Add an event listener for the 'load' event
 window.addEventListener("load", function () {
-    // Show the content once the page has fully loaded
-    document.body.style.visibility = "visible";
+	// Show the content once the page has fully loaded
+	document.body.style.visibility = "visible";
 
-    // Apply the theme based on the user's preference
-    if (themePreference != null) {
-        cookiesAccepted = true;
-        applyTheme(currentTheme);
-    } else {
-        applyTheme("https://deepsealemons.co.uk/styles/light.css");
-    }
+	// Apply the theme based on the user's preference
+	if (themePreference != null) {
+		cookiesAccepted = true;
+		applyTheme(currentTheme);
+	} else {
+		applyTheme("https://deepsealemons.co.uk/styles/light.css");
+	}
 });
 
 function toggleTheme() {
