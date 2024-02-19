3178717aeaba0f9ef39602503ce25685c06b7ebd
Toby <83547635+Toby-420@users.noreply.github.com>
2024-02-19 16:21:11 +0000

    Update styles

--- javascript/cookie.js
+++ javascript/cookie.js
@@ -1,10 +1,10 @@
 // Technically not cookie but who gives a shit
-let cookiesAccepted = false;
 let themePreference = localStorage.getItem("themePreference");
 let currentTheme = themePreference || "styles/light.css";
   
 const hiringSVGContainer = document.getElementById("hiringSVG");
 const jobsSVGContainer = document.getElementById("jobsSVG");
+const X_Logo = document.getElementById("X_Logo");
 
 if (themePreference != null) {
 	cookiesAccepted = true;
@@ -13,33 +13,10 @@ if (themePreference != null) {
 	applyTheme("styles/light.css");
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-  applyTheme(currentTheme);
-  if (!themePreference) {
-    document.getElementById("cookie-consent").style.display = "block";
-  }
-});
-
 function toggleTheme() {
   currentTheme = currentTheme === "styles/light.css" ? "styles/dark.css" : "styles/light.css";
   applyTheme(currentTheme);
-  console.log(cookiesAccepted);
-  if (cookiesAccepted) {
-    updateThemePreference(currentTheme);
-  }
-}
-
-function acceptCookie() {
-  document.getElementById("cookie-consent").style.display = "none";
-  cookiesAccepted = true;
   updateThemePreference(currentTheme);
-}
-
-function rejectCookie() {
-  document.getElementById("cookie-consent").style.display = "none";
-  localStorage.removeItem("themePreference");
-  themePreference = null; // Reset themePreference variable
-  cookiesAccepted = false;
 }
 
 function updateThemePreference(theme) {
@@ -51,10 +28,12 @@ function applyTheme(theme) {
   document.getElementById("css_standard").setAttribute("href", theme);
   
   if (currentTheme === "styles/light.css") {
-	  hiringSVGContainer.innerHTML = `<img class="svg preview littleimg space" src="images/icons/light-calendar.svg" alt="Calendar Icon (For booking)">`;
-	  jobsSVGContainer.innerHTML = `<img class="svg preview littleimg space" src="images/icons/light-hired.svg" alt="Hired Icon (For jobs)">`;
+	  hiringSVGContainer.setAttribute("src", "images/icons/light-calendar.svg");
+	  jobsSVGContainer.setAttribute("src", "images/icons/light-hired.svg");
+	  X_Logo.setAttribute("src", "images/icons/x-black.png");
   } else {
-	  hiringSVGContainer.innerHTML = `<img class="svg preview littleimg space" src="images/icons/dark-calendar.svg" alt="Calendar Icon (For booking)">`;
-	  jobsSVGContainer.innerHTML = `<img class="svg preview littleimg space" src="images/icons/dark-hired.svg" alt="Hired Icon (For jobs)">`;
+	  hiringSVGContainer.setAttribute("src", "images/icons/dark-calendar.svg");
+	  jobsSVGContainer.setAttribute("src", "images/icons/dark-hired.svg");
+	  X_Logo.setAttribute("src", "images/icons/x-white.png");
   }
 }
